53260b33cf113db8a3a026ccebbb1c1e22780f88
a <a@as-Virtual-Machine.local>
2023-06-01 18:24:37 -0700

    Update parseComment function in Main.hs

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,10 +1,11 @@
 module Main (main) where
 
+import Control.Lens ((^?), ix)
 import Data.Aeson.Types (FromJSONKey(..), FromJSONKeyFunction(..), Parser)
 import Data.HashMap.Strict (HashMap)
 import Data.Hashable (Hashable(..))
 import Data.List (isSuffixOf, isPrefixOf)
-import Data.Text (Text, unpack)
+import Data.Text (Text, unpack, splitOn, pack)
 import Data.Yaml (decodeFileEither, ParseException, prettyPrintParseException, FromJSON(..), withText)
 import GHC.Generics (Generic)
 import Network.URI (URI, parseURI, uriAuthority, uriPath, uriRegName, uriToString)
@@ -12,35 +13,35 @@ import Prelude
 import System.Directory (getHomeDirectory)
 import System.FilePath ((</>))
 
-data UrlConfig = UrlConfig
+data CommentConfig = CommentConfig
   { text :: Text
   , threshold :: Double
   } deriving (Show, Eq, Generic)
 
-instance FromJSON UrlConfig
+instance FromJSON CommentConfig
 
-parseComment :: Text -> Parser Comment
-parseComment t = case parseURI (unpack t) of
+parseCommentURI :: Text -> Parser CommentURI
+parseCommentURI t = case parseURI (unpack t) of
   Just uri -> case uriAuthority uri of
-    Just auth -> if "reddit.com" `isSuffixOf` uriRegName auth && "/r/" `isPrefixOf` uriPath uri
-      then pure (Comment uri)
+    Just auth -> if "reddit.com" `isSuffixOf` uriRegName auth && "/r/" `isPrefixOf` uriPath uri && (splitOn "/" (pack (uriPath uri)) ^? ix 3) == Just "comments"
+      then pure (CommentURI uri)
       else fail "Invalid URI: not a Reddit comment"
     Nothing -> fail "Invalid URI: no authority"
   Nothing  -> fail "Invalid URI: could not parse URI"
 
-newtype Comment = Comment URI
+newtype CommentURI = CommentURI URI
   deriving (Show, Eq, Generic)
 
-instance FromJSON Comment where
-  parseJSON = withText "URI" parseComment
+instance FromJSON CommentURI where
+  parseJSON = withText "URI" parseCommentURI
         
-instance FromJSONKey Comment where
-  fromJSONKey = FromJSONKeyTextParser parseComment
+instance FromJSONKey CommentURI where
+  fromJSONKey = FromJSONKeyTextParser parseCommentURI
 
-instance Hashable Comment where
-  hashWithSalt salt (Comment path) = hashWithSalt salt (uriToString id path "")
+instance Hashable CommentURI where
+  hashWithSalt salt (CommentURI path) = hashWithSalt salt (uriToString id path "")
 
-type Config = HashMap Text (HashMap Comment UrlConfig)
+type Config = HashMap Text (HashMap CommentURI CommentConfig)
 
 parseConfigFile :: FilePath -> IO (Either ParseException Config)
 parseConfigFile = decodeFileEither
