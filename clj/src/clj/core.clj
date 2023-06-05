071f4ad3e40d811a96d826e67e10949821e6f5d9
a <a@as-Virtual-Machine.local>
2023-06-05 00:35:26 -0700

    Add libpython-clj dependency to clj project

--- clj/src/clj/core.clj
+++ clj/src/clj/core.clj
@@ -1,5 +1,8 @@
 (ns clj.core
-  (:gen-class))
+  (:gen-class)
+  (:require [libpython-clj2.python :as py]))
+
+(py/initialize! :python-executable "../.venv/bin/python")
 
 (defn -main
   "I don't do a whole lot ... yet."
