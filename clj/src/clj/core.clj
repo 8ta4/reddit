0878fd52f6925414a51232bd0d732f8707fe98b6
a <a@as-Virtual-Machine.local>
2023-06-05 18:01:37 -0700

    Add get-scores function to clj.core

--- clj/src/clj/core.clj
+++ clj/src/clj/core.clj
@@ -9,6 +9,15 @@
 (def model
   (SentenceTransformer "paraphrase-MiniLM-L6-v2" :cache_folder "models"))
 
+(py/$a model encode ["Hello world!"] :convert_to_tensor true)
+
+(defn get-scores
+  [examples query]
+  (let [embeddings (py/$a model encode examples :convert_to_tensor true)
+        query-embeddings (py/$a model encode [query] :convert_to_tensor true)
+        scores (py/$a util cos_sim query-embeddings embeddings)]
+    (py/$a scores tolist)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
