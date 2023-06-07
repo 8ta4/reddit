cf1d4c9813919c82f24191681bdcee603dfbd5af
a <a@as-Virtual-Machine.local>
2023-06-07 02:44:34 -0700

    Add command line argument parsing using optparse-applicative

--- hs/app/Main.hs
+++ hs/app/Main.hs
@@ -5,14 +5,31 @@ import Data.List (sortOn)
 import Data.Maybe (catMaybes, mapMaybe)
 import Data.Ord (Down (Down))
 import Data.Yaml (prettyPrintParseException)
+import Options.Applicative (Alternative ((<|>)), Parser, argument, command, execParser, fullDesc, header, helper, info, metavar, progDesc, str, subparser, (<**>))
 import Reddit
 import System.Directory (getHomeDirectory)
 import System.FilePath ((</>))
 import Text.Feed.Query (getFeedItems)
 import Prelude
 
-main :: IO ()
-main = do
+data Command
+  = Default
+  | Scores String
+
+commandParser :: Parser Command
+commandParser =
+  pure Default
+    <|> subparser
+      ( command
+          "scores"
+          ( info
+              (Scores <$> argument str (metavar "URL"))
+              (progDesc "Calculate similarity scores for a specific post")
+          )
+      )
+
+defaultAction :: IO ()
+defaultAction = do
   homeDir <- getHomeDirectory
   let configFile = homeDir </> ".config" </> "reddit" </> "config.yaml"
   putStrLn configFile
@@ -30,3 +47,16 @@ main = do
 
       print m
       traverse_ (printPostURL m) postURLs
+
+scoresAction :: String -> IO ()
+scoresAction url = do
+  putStrLn $ "Calculating scores for: " ++ url
+
+-- Implement your logic for calculating scores here
+
+main :: IO ()
+main = do
+  cmd <- execParser $ info (commandParser <**> helper) (fullDesc <> header "reddit - CLI for Reddit")
+  case cmd of
+    Default -> defaultAction
+    Scores url -> scoresAction url
